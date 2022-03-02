e7f7ff66ce2411e4f88809c222edc02e65926c71
Maddi Wade <Madelinenwade@gmail.com>
2022-03-02 15:46:50 -0500

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>();
+           
+
+            videoGames.Add("Call Of Duty");
+            videoGames.Add("Halo");
+            videoGames.Add("Rainbow 6");
+            videoGames.Add("OverWatch");
+            videoGames.Add("Fallout");
+            videoGames.Add("Madden");
+
+            
+            List<string> gameLength = videoGames.OrderBy(videoGames => videoGames).ToList();
+            foreach(var game in gameLength)
+                Console.WriteLine(game);
+        }
+    }
+}
